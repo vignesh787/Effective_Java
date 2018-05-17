b3b6cff0e08ce96933f296218f39cf596bfa2257
vignesh787 <vignesh787@gmail.com>
2018-05-17 06:53:29 +0530

    Consider Builder when faced with many constructors

---
+++ src/com/part7/Stack.java
@@ -0,0 +1,42 @@
+package com.part7;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class Stack {
+
+    Object[] entries;
+
+    private int size = 0;
+
+    private static final int INITIAL_SIZE = 16;
+
+    /**
+     * Constructor
+     */
+    public Stack(){
+        entries = new Object[INITIAL_SIZE];
+
+    }
+
+    public void push(Object e){
+       // check for size before pushing
+        ensureSize();
+            entries[size++] = e;
+    }
+
+    public Object pop(){
+        if(size == 0 ){
+            throw new EmptyStackException();
+        }
+        Object result = entries[--size];
+        entries[size] = null; // Eleminating obselete references
+        return result;
+    }
+
+    public void ensureSize(){
+        if(entries.length == size){
+            Arrays.copyOf(entries,2*(size + 1));
+    }
+
+}
